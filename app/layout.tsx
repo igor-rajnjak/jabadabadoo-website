8a6509f2b7992f79b908773613c03b4e7fd96a49
Igor Rajnjak <igorrajnjak@192.168.1.6>
2025-12-08 10:35:52 +0100

    Move Google Analytics tag to head section as per Google recommendations

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { CONTACT, SEO } from "@/lib/constants";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 export const metadata: Metadata = {
   title: SEO.title,
@@ -42,6 +41,21 @@ export default function RootLayout({
   return (
     <html lang="sr">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-N2557RR5NG"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-N2557RR5NG');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -90,7 +104,6 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased">
-        <GoogleAnalytics />
         {children}
         <SpeedInsights />
       </body>
